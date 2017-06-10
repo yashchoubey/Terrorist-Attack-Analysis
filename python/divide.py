a544d715e520df7b3a17d15a0e6301bac53341f5
yashchoubey <yashchoubey2009@gmail.cm>
2017-06-10 17:51:24 +0530

    initial commit

--- python/divide.py
+++ python/divide.py
@@ -1,7 +1,7 @@
 import csv
 from itertools import groupby
-import operator
-for key, rows in groupby(csv.reader(open("indiadata.csv")),operator.itemgetter(5)):
+
+for key, rows in groupby(csv.reader(open("indiadata.csv")),lambda row: row[5]):
     with open("%s.csv" % key, "w") as output:
         for row in rows:
             output.write(",".join(row) + "\n")
